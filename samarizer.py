7d8454f0732ad53bdab9440b09590d0bc451a56d
Bushra Akram <141483100+BushraAkram111@users.noreply.github.com>
2024-07-14 12:54:04 +0500

    Update samarizer.py

--- samarizer.py
+++ samarizer.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import sumy
-import gensim
-import scipy
-
-st.write(f"sumy version: {sumy.__version__}")
-st.write(f"gensim version: {gensim.__version__}")
-st.write(f"scipy version: {scipy.__version__}")
-
-# Your existing imports and code
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.text_rank import TextRankSummarizer
@@ -22,17 +13,14 @@ text_input = st.text_area('Text', '')
 # Summarize Button
 if st.button('Summarize'):
     if text_input:
-        try:
-            parser = PlaintextParser.from_string(text_input, Tokenizer('english'))
-            summarizer = TextRankSummarizer()
-            summary = summarizer(parser.document, 2)  # Number of sentences in summary
-            summary_text = ' '.join([str(sentence) for sentence in summary])
-            if summary_text:
-                st.subheader('Summary:')
-                st.write(summary_text)
-            else:
-                st.write('Text is too short to summarize.')
-        except Exception as e:
-            st.write(f'An error occurred: {e}')
+        parser = PlaintextParser.from_string(text_input, Tokenizer('english'))
+        summarizer = TextRankSummarizer()
+        summary = summarizer(parser.document, 2)  # Number of sentences in summary
+        summary_text = ' '.join([str(sentence) for sentence in summary])
+        if summary_text:
+            st.subheader('Summary:')
+            st.write(summary_text)
+        else:
+            st.write('Text is too short to summarize.')
     else:
         st.write('Please enter some text to summarize.')
